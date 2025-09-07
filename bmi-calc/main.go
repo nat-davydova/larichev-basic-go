7f41b8a414432ed3000dd2fd1ba6e603e96efe9c
Nat Davydova <natti.davydova@gmail.com>
2025-09-07 18:19:58 +0300

    module 4-2: change height input in m to cm in BMI calc

--- bmi-calc/main.go
+++ bmi-calc/main.go
@@ -8,16 +8,18 @@ import (
 func main() {
 	const BMIPow = 2
 
-	var userHeightMeters float64
+	var userHeightCm float64
 	var userWeightKg float64
 
 	fmt.Println("BMI calculator")
 
-	fmt.Println("Enter your user height in meters")
-	fmt.Scan(&userHeightMeters)
+	fmt.Println("Enter your user height in cm")
+	fmt.Scan(&userHeightCm)
 
 	fmt.Println("Enter your user weight in kg")
 	fmt.Scan(&userWeightKg)
+
+	var userHeightMeters = userHeightCm / 100
 
 	BMI := userWeightKg / math.Pow(userHeightMeters, BMIPow)
 	fmt.Printf("Your BMI is: %.0f", BMI)
