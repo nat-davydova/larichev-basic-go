0cc8c57c8875b8773ac36f491a37765a0d52cfac
Nat Davydova <natti.davydova@gmail.com>
2025-09-15 17:32:18 +0300

    feat: add separate getting target currency function

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -29,12 +30,15 @@ func getUserInput() (string, string, float64) {
 	var moneyAmountErr error
 
 	var targetCurrency string
+	var targetCurrencyErr error
 
 	for {
 		inputCurrency, inputCurrencyErr = getInputCurrency()
 
 		if inputCurrencyErr == nil {
 			break
+		} else {
+			fmt.Printf("Error getting input currency - %v\n", inputCurrencyErr)
 		}
 	}
 
@@ -43,11 +47,20 @@ func getUserInput() (string, string, float64) {
 
 		if moneyAmountErr == nil {
 			break
+		} else {
+			fmt.Printf("Error getting input currency - %v\n", moneyAmountErr)
 		}
 	}
 
-	fmt.Println("Enter your target currency:")
-	fmt.Scan(&targetCurrency)
+	for {
+		targetCurrency, targetCurrencyErr = getTargetCurrency(inputCurrency)
+
+		if targetCurrencyErr == nil {
+			break
+		} else {
+			fmt.Printf("Error getting target currency - %v\n", targetCurrencyErr)
+		}
+	}
 
 	return inputCurrency, targetCurrency, moneyAmount
 }
@@ -58,8 +71,10 @@ func getInputCurrency() (string, error) {
 	fmt.Println("Enter your init currency (RUB, USD, EUR):")
 	fmt.Scan(&initCurrency)
 
-	if initCurrency != "RUB" && initCurrency != "USD" && initCurrency != "EUR" {
-		return "", errors.New("invalid init currency")
+	initCurrencyFormatted := strings.ToLower(initCurrency)
+
+	if initCurrencyFormatted != "rub" && initCurrencyFormatted != "usd" && initCurrencyFormatted != "eur" {
+		return "", errors.New("invalid init currency, you're allowed to enter only one of (RUB, USD, EUR)")
 	}
 
 	return initCurrency, nil
@@ -76,8 +91,43 @@ func getMoneyAmount() (float64, error) {
 	if err == nil {
 		return parsed, nil
 	} else {
-		return -1, errors.New("invalid money amount")
+		return -1, errors.New("invalid money amount, you're allowed to enter only numbers")
 	}
+}
+
+func getTargetCurrency(inputCurrency string) (string, error) {
+	var targetCurrency string
+	var description string
+
+	inputCurrencyFormatted := strings.ToLower(inputCurrency)
+
+	switch inputCurrencyFormatted {
+	case "eur":
+		description = "(USD, RUB)"
+	case "usd":
+		description = "(EUR, RUB)"
+	case "rub":
+		description = "(EUR, USD)"
+	default:
+		return "", errors.New("invalid init currency")
+	}
+
+	fmt.Printf("Enter your target currency %v:", description)
+	fmt.Scan(&targetCurrency)
+
+	targetCurrencyFormatted := strings.ToLower(targetCurrency)
+
+	switch {
+	case inputCurrencyFormatted == "eur" && (targetCurrencyFormatted == "usd" || targetCurrencyFormatted == "rub"):
+	case inputCurrencyFormatted == "usd" && (targetCurrencyFormatted == "eur" || targetCurrencyFormatted == "rub"):
+	case inputCurrencyFormatted == "rub" && (targetCurrencyFormatted == "eur" || targetCurrencyFormatted == "usd"):
+		return targetCurrency, nil
+	default:
+		errorText := fmt.Sprintf("invalid target currency, you're allowed to enter only one of %v", description)
+		return "", errors.New(errorText)
+	}
+
+	return targetCurrency, nil
 }
 
 func convertCurrencies(initCurrency string, targetCurrency string, moneyAmount float64) float64 {
