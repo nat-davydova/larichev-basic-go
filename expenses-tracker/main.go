359858e566761f595b5026247800ff41e18ed48c
Nat Davydova <natti.davydova@gmail.com>
2025-09-29 19:46:22 +0300

    feat: add basic transactions input (in progress)

--- expenses-tracker/main.go
+++ expenses-tracker/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	var transactions []float64
+}
+
+func getTransactionInput() (float64, error) {
+	var transaction string
+
+	fmt.Println("Enter your transaction amount:")
+	fmt.Scan(&transaction)
+
+	transactionTrimmed := strings.TrimSpace(transaction)
+	transactionFormatted := strings.ReplaceAll(transactionTrimmed, ",", ".")
+
+	parsed, err := strconv.ParseFloat(transactionFormatted, 64)
+
+	if err != nil || parsed == 0 {
+		return 0, errors.New("invalid transaction amount: should be a number and not equal to 0")
+	}
+
+	return parsed, nil
 }
